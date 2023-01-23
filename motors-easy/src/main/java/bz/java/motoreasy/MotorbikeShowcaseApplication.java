e54da0c880a6af9e5cb5a42aec0c1b6bde42016c
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-01-23 15:07:44 -0300

    Blank project

---
+++ motors-easy/src/main/java/bz/java/motoreasy/MotorbikeShowcaseApplication.java
@@ -0,0 +1,13 @@
+package bz.java.motoreasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorbikeShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorbikeShowcaseApplication.class, args);
+	}
+
+}
---
+++ motors-easy/src/test/java/bz/java/motoreasy/MotorbikeShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package bz.java.motoreasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorbikeShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
