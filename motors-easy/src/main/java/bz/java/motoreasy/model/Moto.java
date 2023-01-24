cc015c0819eb4b43e1b755f68edec09af2286037
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-01-24 18:46:54 -0300

    Main model classes created. More may be added in the future.

---
+++ motors-easy/src/main/java/bz/java/motoreasy/model/Moto.java
@@ -0,0 +1,75 @@
+package bz.java.motoreasy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Moto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String modelo;
+    private int cilindradas;
+    private double preco;
+    private boolean automatica;
+
+    public Moto() {
+    }
+
+    public Moto(long id, String modelo, int cilindradas, double preco, boolean automatica) {
+        this.id = id;
+        this.modelo = modelo;
+        this.cilindradas = cilindradas;
+        this.preco = preco;
+        this.automatica = automatica;
+    }
+
+    public Moto(String modelo, int cilindradas, double preco, boolean automatica) {
+        this.modelo = modelo;
+        this.cilindradas = cilindradas;
+        this.preco = preco;
+        this.automatica = automatica;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCilindradas() {
+        return cilindradas;
+    }
+
+    public void setCilindradas(int cilindradas) {
+        this.cilindradas = cilindradas;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public boolean isAutomatica() {
+        return automatica;
+    }
+
+    public void setAutomatica(boolean automatica) {
+        this.automatica = automatica;
+    }
+}
---
+++ motors-easy/src/main/java/bz/java/motoreasy/model/Usuario.java
@@ -0,0 +1,83 @@
+package bz.java.motoreasy.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome, email, login, senha;
+    @OneToMany
+    private List<Moto> listaDesejo;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nome, String email, String login, String senha, List<Moto> listaDesejo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.login = login;
+        this.senha = senha;
+        this.listaDesejo = listaDesejo;
+    }
+
+    public Usuario(String nome, String email, String login, String senha, List<Moto> listaDesejo) {
+        this.nome = nome;
+        this.email = email;
+        this.login = login;
+        this.senha = senha;
+        this.listaDesejo = listaDesejo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public List<Moto> getListaDesejo() {
+        return listaDesejo;
+    }
+
+    public void setListaDesejo(List<Moto> listaDesejo) {
+        this.listaDesejo = listaDesejo;
+    }
+}
