e5af8c049f05f973f67f3403e066473069d0c312
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-01-25 19:32:24 -0300

    Repository Interfaces created

---
+++ motors-easy/src/main/java/bz/java/motoreasy/repository/MotoRepo.java
@@ -0,0 +1,7 @@
+package bz.java.motoreasy.repository;
+
+import bz.java.motoreasy.model.Moto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotoRepo extends JpaRepository<Moto, Long> {
+}
---
+++ motors-easy/src/main/java/bz/java/motoreasy/repository/UsuarioRepo.java
@@ -0,0 +1,7 @@
+package bz.java.motoreasy.repository;
+
+import bz.java.motoreasy.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepo extends JpaRepository<Usuario, Long> {
+}
