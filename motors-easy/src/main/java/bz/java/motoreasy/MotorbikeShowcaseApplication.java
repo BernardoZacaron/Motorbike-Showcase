29d4c3d8227432dafaf6aae96a2443cd44b4966b
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-02-06 19:16:09 -0300

    Creating some bikes to populate db and wishlist page adjustment

--- motors-easy/src/main/java/bz/java/motoreasy/MotorbikeShowcaseApplication.java
+++ motors-easy/src/main/java/bz/java/motoreasy/MotorbikeShowcaseApplication.java
@@ -1,13 +1,30 @@
 package bz.java.motoreasy;
 
+import bz.java.motoreasy.model.Moto;
+import bz.java.motoreasy.repository.MotoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MotorbikeShowcaseApplication {
+public class MotorbikeShowcaseApplication implements CommandLineRunner {
+	@Autowired
+	MotoRepo motoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MotorbikeShowcaseApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Moto m1 = new Moto("Twister",200, 18000, false);
+		motoRepo.save(m1);
+		Moto m2 = new Moto("CG Titan",160, 15700, false);
+		motoRepo.save(m2);
+		Moto m3 = new Moto("Honda XRE",190, 18700, false);
+		motoRepo.save(m3);
+		Moto m4 = new Moto("Dafra Citycom",300, 25400, true);
+		motoRepo.save(m4);
+	}
 }
