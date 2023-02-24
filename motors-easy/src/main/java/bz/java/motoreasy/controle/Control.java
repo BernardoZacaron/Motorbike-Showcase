b02bb7263b81ba7febe4b6e22d8563a4268c6bb2
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-02-24 20:21:07 -0300

    Greetings now getting logged in user name

--- motors-easy/src/main/java/bz/java/motoreasy/controle/Control.java
+++ motors-easy/src/main/java/bz/java/motoreasy/controle/Control.java
@@ -9,6 +9,7 @@ import bz.java.motoreasy.repository.UsuarioRepo;
 import bz.java.motoreasy.seguranca.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import javax.ws.rs.NotFoundException;
 import java.util.ArrayList;
@@ -44,7 +46,11 @@ public class Control {
 
     //Aberto
     @GetMapping({"/", "/home"})
-    public String callHomePage() {
+    public String callHomePage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String nomeUsuario = authentication.getName();
+
+        model.addAttribute("nomeUsuario", nomeUsuario);
         return "index";
     }
 
