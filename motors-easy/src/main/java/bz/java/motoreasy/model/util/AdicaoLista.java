86606eafaf8c9efe879b715efe14e411cfe19240
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-03-21 15:48:47 -0300

    Restructuring of classes to be compatible with the database

---
+++ motors-easy/src/main/java/bz/java/motoreasy/model/util/AdicaoLista.java
@@ -0,0 +1,39 @@
+//package bz.java.motoreasy.model.util;
+//
+//import bz.java.motoreasy.model.ListaFavoritos;
+//import bz.java.motoreasy.model.Moto;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Table;
+//
+//@Table(name = "LISTA_FAVORITOS_MOTOS")
+//public class ListaFavoritosMotos {
+//    @Column(name = "LISTA_FAVORITOS_ID")
+//    private ListaFavoritos listaFavoritosId;
+//    @Column(name = "MOTOS_ID")
+//    private Moto motosId;
+//
+//    public ListaFavoritosMotos() {
+//    }
+//
+//    public ListaFavoritosMotos(ListaFavoritos listaFavoritosId, Moto motosId) {
+//        this.listaFavoritosId = listaFavoritosId;
+//        this.motosId = motosId;
+//    }
+//
+//    public ListaFavoritos getListaFavoritosId() {
+//        return listaFavoritosId;
+//    }
+//
+//    public void setListaFavoritosId(ListaFavoritos listaFavoritosId) {
+//        this.listaFavoritosId = listaFavoritosId;
+//    }
+//
+//    public Moto getMotosId() {
+//        return motosId;
+//    }
+//
+//    public void setMotosId(Moto motosId) {
+//        this.motosId = motosId;
+//    }
+//}
