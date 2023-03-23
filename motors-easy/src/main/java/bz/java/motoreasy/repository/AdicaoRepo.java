8a6fd122e8c59aa008d3e00746d657c0eeab308a
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-03-23 16:28:04 -0300

    Wishlist actions now fully functional

--- motors-easy/src/main/java/bz/java/motoreasy/repository/AdicaoRepo.java
+++ motors-easy/src/main/java/bz/java/motoreasy/repository/AdicaoRepo.java
@@ -4,6 +4,7 @@ import bz.java.motoreasy.model.Moto;
 import bz.java.motoreasy.model.Usuario;
 import bz.java.motoreasy.model.util.AdicaoLista;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -11,7 +12,8 @@ import java.util.List;
 
 public interface AdicaoRepo extends JpaRepository<AdicaoLista, Long> {
 
-    @Query("DELETE FROM AdicaoLista WHERE USUARIO = ?1 AND MOTO = ?2")
+    @Modifying
+    @Query(value = "DELETE FROM AdicaoLista WHERE USUARIO_ID = ?1 AND MOTO_ID = ?2")
     void removerMotoDaLista(long usuarioId, long motoId);
 
     List<AdicaoLista> findByUsuario(Usuario usuario);
